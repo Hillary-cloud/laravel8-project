412ce53aea713a3315d284fa499948ed5aa11ba7
Obiora Hillary <obicj97@gmail.com>
2021-06-17 16:45:28 -0700

    init

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+
+    public function __construct(){
+        $this->middleware(['auth'])->only(['store','destroy']);
+    }
+    public function index(){
+
+        $posts = Post::orderBy('created_at', 'desc')->with(['user','likes'])->paginate(3);//or Post::latest()->paginate(3). to get all the posts
+
+        return view('posts.index', [
+            'posts'=>$posts
+        ]);
+    }
+    public function store(Request $request){
+        $this->validate($request, [
+            'body'=>'required',
+
+        ]);
+
+        $request->user()->posts()->create($request->only('body'));
+        return back();
+    }
+    public function destroy(Post $post){
+        $post->delete();
+        return back();
+    }
+}
