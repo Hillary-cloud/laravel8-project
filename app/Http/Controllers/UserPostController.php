d2004056372ae817513a0c276c51a64eb0848192
hillary-cloud <obicj97@gmail.com>
2021-07-09 16:31:13 -0700

    init

--- app/Http/Controllers/UserPostController.php
+++ app/Http/Controllers/UserPostController.php
@@ -8,7 +8,7 @@ use App\Models\User;
 class UserPostController extends Controller
 {
     public function index(User $user){
-        $posts = $user->posts()->with(['user','likes'])->paginate(10);
+        $posts = $user->posts()->latest()->with(['user','likes'])->paginate(10);
         return view('users.posts.index',[
             'user'=>$user,
             'posts'=>$posts,
